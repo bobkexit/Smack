5e2a65cb84c88e24142e0948dc294998e20758c7
Bob Kexit <bobkexit@gmail.com>
2017-12-06 16:10:58 +0300

    setup Login UI

---
+++ Smack/Controllers/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Николай Маторин on 06.12.2017.
+//  Copyright © 2017 Николай Маторин. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Николай Маторин on 06.12.2017.
+//  Copyright © 2017 Николай Маторин. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
