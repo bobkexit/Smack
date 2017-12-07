59148a0710f912a3405c9f6f098cf61713bda9e7
Bob Kexit <bobkexit@gmail.com>
2017-12-07 15:19:43 +0300

    Register user, added .gitignore file

--- Smack/Controllers/CreateAccountVC.swift
+++ Smack/Controllers/CreateAccountVC.swift
@@ -10,13 +10,37 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-   
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let email = emailTxt.text, emailTxt.text != "" else { return  }
+        guard let pass = passTxt.text, passTxt.text != "" else { return  }
+
+        AuthService.instance.regesterUser(email: email, password: pass) { (success) in
+            if success {
+                print("regestered user!")
+            }
+        }
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+    }
+    
+    
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
---
+++ Smack/Services/AuthService.swift
@@ -0,0 +1,67 @@
+//
+//  AuthService.swift
+//  Smack
+//
+//  Created by Николай Маторин on 07.12.2017.
+//  Copyright © 2017 Николай Маторин. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AuthService {
+   
+    static let instance = AuthService()
+    
+    let defaults = UserDefaults.standard
+    
+    var isLoggedIn: Bool {
+        get {
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set {
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToken: String {
+        get {
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    var userEmail: String {
+        get {
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    func regesterUser(email: String, password: String, complition: @escaping CompletionHandler) {
+        
+        let lowerCaseEmail = email.lowercased()
+        
+        let header = [
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+            if response.result.error == nil {
+                complition(true)
+            } else {
+                complition(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+}
