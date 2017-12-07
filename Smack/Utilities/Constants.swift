6d8b89c8557050ee1301abe9945770de0a2adc51
Bob Kexit <bobkexit@gmail.com>
2017-12-07 20:36:37 +0300

    choose an avatar

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -19,6 +19,8 @@ let URL_USER_ADD = "\(BASE_URL)user/add"
 // Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
+let TO_AVATAR_PICKER = "toAvatarPicker"
+
 let UNWIND = "unwindToChannel"
 
 //User Defaults
